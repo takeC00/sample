bc96acd0ec2afddf55673de5dd2167986ed2c8e0
takeC00 <take.614@icloud.com>
2023-06-28 08:31:35 +0900

    Controllerでview返す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,22 +9,6 @@ class HelloController extends Controller
 {
     public function index(Request $request, Response $response)
     {
-        $html = <<<EOF
-        <html>
-        <head>
-        <title>Hello/Index</title>
-        <style>
-            body { font-size:16px; color:#999; }
-            h1 {font-size:120pt; text-align:right; color:#fafafa; margin:-50px 0px -120px 0px }
-        </style>
-        </head>
-        <body>
-
-        </body>
-        </html>s
-        EOF;
-
-        $response->setContent($html);
-        return $response;
+        return view('hello.index');
     }
 }
