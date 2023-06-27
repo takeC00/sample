7118abbd948352fa27a31361671d410a3eeb606d
takeC00 <take.614@icloud.com>
2023-06-27 21:26:17 +0900

    first

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+    public function index(Request $request, Response $response)
+    {
+        $html = <<<EOF
+        <html>
+        <head>
+        <title>Hello/Index</title>
+        <style>
+            body { font-size:16px; color:#999; }
+            h1 {font-size:120pt; text-align:right; color:#fafafa; margin:-50px 0px -120px 0px }
+        </style>
+        </head>
+        <body>
+            
+        </body>
+        </html>
+        EOF;
+
+        $response->setContent($html);
+        return $response;
+    }
+}
