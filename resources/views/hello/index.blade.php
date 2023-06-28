d9f3a9b3e1f8d5733b9f13760ad6ddb0afb014dd
takeC00 <take.614@icloud.com>
2023-06-28 21:10:00 +0900

    レイアウトview

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,41 +1,17 @@
-<!DOCTYPE html>
-{{--<h1>変数をviewに渡す</h1>
-@if ($msg)
-<p>$msgを表示</p>
-<p>{{$msg}}</p>
-@else
-<p>何か入力してください</p>
-@endif--}}
+@extends('layouts.helloapp')
 
-{{--<p>id={{$id}}</p>--}}
-<form method="POST" action="/hello">
-    @csrf
-    <input type="text" name="msg">
-    <input type="submit">
-</form>
+@section('title', 'Index')
 
-{{ '<h2>エスケープされる</h2>' }}
-{!! '<h2>エスケープされない</h2>' !!}
+@section('menubar')
+    @parent
+    インデックスページ ※セクション(menubar)
+@endsection
 
-<ol>
-@for($i=1; $i<100; $i++)
-@if($i%2 == 1)
-    @continue
-@elseif ($i<=10)
-<li>No, {{$i}}
-@else
-    @break
-@endif
-@endfor
-</ol>
+@section('content')
+    <p>本文のコンテンツ ※セクション(content)</p>
+    <p>必要なだけ記述できる ※セクション(content)</p>
+@endsection
 
-
-@foreach($data as $item)
-@if($loop->first)
-<P>※データ一覧</P><ul>
-@endif
-<li>No,{{$loop->iteration}}.{{$item}}</li>
-@if($loop->last)
-</ul><p>------ここまで</p>
-@endif
-@endforeach
+@section('footer')
+    copyright 2023 ※セクション(footer)
+@endsection
