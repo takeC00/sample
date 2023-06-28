6491ab9abeaacf128c7de7199c821632c80c2a2e
takeC00 <take.614@icloud.com>
2023-06-28 21:47:01 +0900

    コンポーネント

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,16 @@
 @section('content')
     <p>本文のコンテンツ ※セクション(content)</p>
     <p>必要なだけ記述できる ※セクション(content)</p>
+
+    @component('components.message')
+        @slot('msg_title')
+        CAUTION!
+        @endslot
+
+        @slot('msg_content')
+        これはメッセージの表示です。
+        @endslot
+    @endcomponent
 @endsection
 
 @section('footer')
