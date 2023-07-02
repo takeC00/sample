410b309f7bc0bf693a79afa6403635e73cba326f
takeC00 <take.614@icloud.com>
2023-07-02 09:50:34 +0900

    バリデーションをControllerからRewuestClassに変更 git push origin develop Q
    wq
    q!
    ;
    
    :
    ;

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -12,14 +13,8 @@ class HelloController extends Controller
         return view('hello.index', ['msg' => 'フォームを入力']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $validate_rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
         return view('hello.index', ['msg'=>'正しく入力されました']);
     }
 }
